86cd04a01b2eb18692dfe90b814943ce7890eb0d
eborius321 <eborius321@gmail.com>
2025-11-23 23:39:15 +0500

    1

--- Tyuiu.TumashenkinYA.Sprint4.Task5.V1.Test/DataServiceTest.cs
+++ Tyuiu.TumashenkinYA.Sprint4.Task5.V1.Test/DataServiceTest.cs
@@ -9,8 +9,9 @@ namespace Tyuiu.TumashenkinYA.Sprint4.Task5.V1.Test
         {
             DataService ds = new DataService();
             int[,] mas2 = new int[5, 5] { { 1, 2, 3, 4, 5 }, { 2, 3, 4, 5, 6 }, { 3, 4, 5, 6, 7 }, { 4, 5, 6, 7, 8 }, { 1, 2, 3, 4, 5 } };
-            int res = ds.Calculate(mas2);
-            int wait
-                }
+            int[,] wait = new int[5, 5] { { 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1 } };
+            int[,] res = ds.Calculate(mas2);
+            CollectionAssert.AreEqual(wait, res);
+        }
     }
 }
